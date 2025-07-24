942fe4db196974ee3cc1c4fbc1418518396e36d2
Kevinn Vannucci <kevinnvannucci0@gmail.com>
2025-07-24 20:03:43 -0300

    deploy

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,7 +1,5 @@
 import 'styled-components';
-import type { ThemeType } from './styles/theme';
-
-type ThemeType = typeof theme;
+import type { ThemeType } from './styles/theme'; 
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
